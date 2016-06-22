12affedc13e51b850e84750f9e6f2b2b22db53a0
Madone <ahamad.jedu@gmail.com>
2016-06-22 12:54:48 +0700

    Bug assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -10,22 +10,12 @@
 namespace firdows\menu\assets;
 
 use yii\web\AssetBundle;
-use Yii;
 
-// set @themes alias so we do not have to update baseUrl every time we change themes
-Yii::setAlias('@themes', Yii::$app->view->theme->baseUrl);
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- *
- * @since 2.0
- *
- * Customized by Nenad Živković
- */
 class AppAsset extends AssetBundle
 {
     
-    public $sourcePath = '@backend/modules/menu/assets';
+    public $sourcePath = '@firdows/menu/assets';
     
     public $css = [
         'css/style.css',
